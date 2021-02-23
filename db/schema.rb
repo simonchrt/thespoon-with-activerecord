563c09f943edf744a1e366d18216f1e76d322eca
Simon Chretien <simon.chretien62@gmail.com>
2021-02-23 09:22:08 +0100

    add address to restaurant

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_23_080639) do
+ActiveRecord::Schema.define(version: 2021_02_23_081843) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
     t.integer "rating"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "address"
   end
 
 end
